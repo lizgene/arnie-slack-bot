e6d5ed97a93ea6a20c2ee2864a3add0be7ae7218
Liz <liz.hubertz@gmail.com>
2016-03-04 18:54:30 +0100

    First move towards richard

--- slack-arniebot/commands/terminate.rb
+++ slack-arniebot/commands/terminate.rb
@@ -5,7 +5,7 @@ module SlackArniebot
         terminated = _match[:expression]
         # might be able to use !channel to get the whole channel
         exercise = File.readlines("exercises.txt").sample.strip.upcase
-        client.say(channel: data.channel, text: "<@#{terminated}> 100 #{exercise} NOW!")
+        client.say(channel: data.channel, text: "<@#{terminated}> 15 #{exercise} Fabuloooous!")
       end
     end
   end
--- spec/slack-arniebot/commands/add_spec.rb
+++ spec/slack-arniebot/commands/add_spec.rb
@@ -4,7 +4,8 @@ describe SlackArniebot::Commands::Add do
   def app
     SlackArniebot::Bot.instance
   end
-  it 'adds a line to the file' do
+  it 'adds a line to the file and responds appropriately' do
     expect(message: "#{SlackRubyBot.config.user} add blahblah", channel: 'exercise').to respond_with_slack_message('Added blahblah to my exercise list')
+    File.read("exercises.txt").should include "blahblah"
   end
 end
