fc047a3f5833b179c1d7ae91723a26241ad0a17c
Liz <liz.hubertz@gmail.com>
2016-03-04 17:58:34 +0100

    some new exercises

--- slack-arniebot/commands/terminate.rb
+++ slack-arniebot/commands/terminate.rb
@@ -3,7 +3,8 @@ module SlackArniebot
     class Terminate < SlackRubyBot::Commands::Base
       command 'terminate' do |client, data, _match|
         terminated = _match[:expression]
-        exercise = File.readlines("exercises.txt").sample.strip
+        # might be able to use !channel to get the whole channel
+        exercise = File.readlines("exercises.txt").sample.strip.upcase
         client.say(channel: data.channel, text: "<@#{terminated}> 100 #{exercise} NOW!")
       end
     end
